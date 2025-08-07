ff29956a1efac3dd415bef47cfbf49c0467f089c
Whitebear <pellehartvigandersen@gmail.com>
2025-08-07 16:31:45 +0200

    initial script to check status

---
+++ check-appointment-status.js
@@ -0,0 +1,62 @@
+const { chromium } = require('playwright');
+require('dotenv').config();
+
+(async () => {
+  const province = process.env.PROVINCE;
+  const passportId = process.env.PASSPORT_ID;
+  const name = process.env.NAME;
+  const yearOfBirth = process.env.YEAR_OF_BIRTH;
+  // const country = process.env.COUNTRY;
+
+  const browser = await chromium.launchPersistentContext('./user-data-dir', { headless: false, ignoreHTTPSErrors: true });
+  const page = await browser.newPage();
+  await page.goto('https://sede.administracionespublicas.gob.es/pagina/index/directorio/icpplus');
+
+  // Click the submit button to proceed from the first page
+  await page.waitForSelector('input#submit');
+  await page.click('input#submit');
+
+  // Select your province
+  await page.waitForSelector('#divProvincias');
+  await page.selectOption('select#form', { label: province });
+  await page.click('#btnAceptar');
+
+  // Select POLICE-NIE ASSIGNMENT appointment
+  await page.waitForSelector('#divGrupoTramites');
+  await page.selectOption('select[name="tramiteGrupo[0]"]', '4031');
+  await page.click('#btnAceptar');
+
+  // Skip requirement page 
+  await page.waitForSelector('#btnEntrar');
+  await page.click('#btnEntrar');
+
+  // Select passport as document type, and fill the form
+  await page.click('#rdbTipoDocPas');
+  await page.waitForSelector('#txtIdCitado', { state: 'visible' });
+  await page.fill('#txtIdCitado', passportId);
+  await page.waitForSelector('#txtDesCitado', { state: 'visible' });
+  await page.fill('#txtDesCitado', name);
+  await page.waitForSelector('#txtAnnoCitado', { state: 'visible' });
+  await page.fill('#txtAnnoCitado', yearOfBirth);
+  await page.selectOption('#txtPaisNac', '108');
+  await page.click('#btnEnviar');
+
+  await page.waitForTimeout(10000);
+
+  // Check appointment status
+  try {
+    const infoText = await page.$eval('p.mf-msg__info', el => el.textContent?.trim() || '');
+    console.log('mf-msg__info paragraph found. Raw text:', infoText);
+    if (infoText.includes('En este momento no hay citas disponibles')) {
+      console.log('Appointment status: unavailable');
+      console.log('Appointment status message:', infoText);
+    } else {
+      console.log('Appointment status: available');
+      console.log('Appointment status message:', infoText);
+    }
+  } catch (err) {
+    console.error('Error while checking appointment status:', err);
+  }
+
+  await browser.close();
+})();
